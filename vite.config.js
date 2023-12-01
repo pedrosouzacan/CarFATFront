c92409c2f11d506fb7382409c9f3a17c91e74cea
pedrosouzacan <pedro.souza@discentes.fat.uerj.br>
2023-12-01 16:01:09 -0300

    Alguma coisa

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/image_upload": 'http://localhost:8080',
-      "/create/": 'http://127.0.0.1:8000/api/v1/profiles',
-      "/profiles/": 'http://127.0.0.1:8000/api/v1',
-    }
-  },
   base: '/Carfatfront/'
 })
